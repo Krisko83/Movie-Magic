97bdf939c6751822184d4a1b7f8bbf8e84d87c2a
Krisko83 <kriss_eidg@yahoo.com>
2026-06-26 17:58:08 +0300

    config handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
+ 
 
 const app = express();
 
+app.engine('hbs', engine());
+app.set('view engine', 'hbs');
+app.set('views', './src/views')
+
 app.get('/', (req, res) => {
-    res.send('Hello there!!!!!')
+    res.render('home', { layout: false})
 })
 
 
