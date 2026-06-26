910fde8581bed16261278fd77308110a6ce43120
Krisko83 <kriss_eidg@yahoo.com>
2026-06-26 19:09:32 +0300

    Setup Router

---
+++ src/controllers/homeController.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+
+const homeController = Router();
+
+homeController.get('/', (req, res) => {
+    res.render('home')
+})
+
+homeController.get('/about', (req, res) => {
+    res.render('about')
+})
+
+homeController.get('/search', (req, res) => {
+    res.render('search')
+})
+
+
+homeController.get('/create', (req, res) => {
+    res.render('create')
+})
+
+
+homeController.get('*url', (req, res) => {
+    res.render('404')
+})
+
+export default homeController;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
- 
+ import homeController from './controllers/homeController.js';
 
 const app = express();
 
@@ -12,25 +12,7 @@ app.set('views', './src/views')
 
 app.use(express.static('./src/public'));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/about', (req, res) => {
-    res.render('about')
-})
-
-app.get('/search', (req, res) => {
-    res.render('search')
-})
+app.use('/', homeController);
 
 
-app.get('/create', (req, res) => {
-    res.render('create')
-})
-
-
-app.get('*url', (req, res) => {
-    res.render('404')
-})
 app.listen(5000, () => console.log('Server is running at http://localhost:5000...'));
